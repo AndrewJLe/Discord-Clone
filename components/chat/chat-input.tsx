c7b8f31b458ae81f0907fa2b6a9e05c795aa9e57
Andrew Le <andrewjacksonle@gmail.com>
2024-01-10 19:21:54 -0800

    file attachment in chat input and pdf support

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -14,6 +14,7 @@ import {
     FormItem,
 
 } from "@/components/ui/form";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -33,6 +34,8 @@ export const ChatInput = ({
     name,
     type,
 }: ChatInputProps) => {
+    const { onOpen } = useModal();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -66,7 +69,7 @@ export const ChatInput = ({
                                 <FormControl>
                                     <div className="relative p-4 pb-6">
                                         <button
-                                            onClick={() => { }}
+                                            onClick={() => onOpen("messageFile", { apiUrl, query })}
                                             type="button"
                                             className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                                         >
